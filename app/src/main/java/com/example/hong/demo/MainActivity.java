b5e3d18b8a38f57d8ff1e16b5789bdc3c8b1d1da
zhangyuhong <289590351@qq.com>
2019-05-02 21:12:48 +0800

    完善框架，路由还没有增加，不过引入了第三方的yokeyword:fragmentation

--- app/src/main/java/com/example/hong/demo/MainActivity.java
+++ app/src/main/java/com/example/hong/demo/MainActivity.java
@@ -1,14 +1,77 @@
 package com.example.hong.demo;
 
-import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.FrameLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
-public class MainActivity extends Activity {
+import com.example.hong.customize.CustomizeMain;
+import com.example.hong.frame.fragment.Frame;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import me.yokeyword.fragmentation.SupportActivity;
+
+public class MainActivity extends SupportActivity {
+
+    @BindView(R.id.fl_content)
+    FrameLayout flContent;
+    @BindView(R.id.bt_kuangjia)
+    RadioButton btKuangjia;
+    @BindView(R.id.bt_zidingyi)
+    RadioButton btZidingyi;
+    @BindView(R.id.bt_texiao)
+    RadioButton btTexiao;
+    @BindView(R.id.bt_yangshi)
+    RadioButton btYangshi;
+    @BindView(R.id.radioGroup)
+    RadioGroup radioGroup;
+    private int position;
+//    SupportFragment fragmenet[4] ={new Frame().in,new };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("zyh", 11111 + "");
+        ButterKnife.bind(this);
+//        Fragmentation.builder()
+//                .stackViewMode(Fragmentation.BUBBLE)
+//                .debug(true)
+//                .install();
+//        Fragmentation.builder()
+//                // 显示悬浮球 ; 其他Mode:SHAKE: 摇一摇唤出   NONE：隐藏
+//                .stackViewMode(Fragmentation.BUBBLE)
+//                .debug(BuildConfig.DEBUG)
+//             .install();
+//        radioGroup.setOnCheckedChangeListener(new MyOnCheckedChangeListen());
     }
+
+    @OnClick({R.id.bt_kuangjia, R.id.bt_zidingyi, R.id.bt_texiao, R.id.bt_yangshi})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.bt_kuangjia:
+//                if (findFragment(Frame.class) == null) {
+                    loadRootFragment(R.id.fl_content, Frame.newInstance());
+//                }
+                break;
+            case R.id.bt_zidingyi:
+//                if (findFragment(CustomizeMain.class) == null) {
+                    loadRootFragment(R.id.fl_content, CustomizeMain.newInstance());
+//                }
+                break;
+            case R.id.bt_texiao:
+                break;
+            case R.id.bt_yangshi:
+                break;
+        }
+    }
+
 }
--- customize/src/main/java/com/example/hong/customize/cehua/fragment/MainContent.java
+++ customize/src/main/java/com/example/hong/customize/cehua/fragment/MainContent.java
@@ -1,38 +1,33 @@
-package com.example.hong.frame.fragment;
+package com.example.hong.customize.cehua.fragment;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.hong.frame.R;
+import com.example.hong.customize.R;
 
 import me.yokeyword.fragmentation.SupportFragment;
 
-
 /**
- * Created by 洪 on 2019/4/30.
+ * Created by 洪 on 2019/5/2.
  */
 
-public class Frame extends SupportFragment{
-    public static Frame newInstance() {
+public class MainContent extends SupportFragment {
+
+    public static MainContent newInstance() {
+        
         Bundle args = new Bundle();
-        Frame fragment = new Frame();
+        
+        MainContent fragment = new MainContent();
         fragment.setArguments(args);
         return fragment;
     }
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.frame_main_fragment, container, false);
+        View view = inflater.inflate(R.layout.customize_cehuamianban_mian, container, false);
         return view;
-    }
-
-    @Override
-    public boolean onBackPressedSupport() {
-        pop();
-        return true;
     }
 }
