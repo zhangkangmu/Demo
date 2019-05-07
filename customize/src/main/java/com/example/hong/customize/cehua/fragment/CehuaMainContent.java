0c583584db8b704503085879e522416f23b7a210
zhangyuhong <289590351@qq.com>
2019-05-08 01:30:47 +0800

    完成QQ侧滑的酷炫效果，但是回调没有增加

--- customize/src/main/java/com/example/hong/customize/cehua/fragment/CehuaMainContent.java
+++ customize/src/main/java/com/example/hong/customize/cehua/fragment/CehuaMainContent.java
@@ -1,13 +1,28 @@
 package com.example.hong.customize.cehua.fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.example.hong.customize.R;
+import com.example.hong.customize.R2;
+import com.example.hong.customize.cehua.other.Constant;
+import com.example.hong.customize.cehua.ui.SlideDragLayout;
+import com.nineoldandroids.view.ViewPropertyAnimator;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 import me.yokeyword.fragmentation.SupportFragment;
 
 /**
@@ -16,19 +31,67 @@ import me.yokeyword.fragmentation.SupportFragment;
 
 public class CehuaMainContent extends SupportFragment {
 
+    @BindView(R2.id.menu_listview)
+    ListView menuListview;
+    @BindView(R2.id.iv_head)
+    ImageView ivHead;
+    @BindView(R2.id.rl_title)
+    RelativeLayout rlTitle;
+    @BindView(R2.id.my_layout)
+    ConstraintLayout myLayout;
+    @BindView(R2.id.slideMenu)
+    SlideDragLayout slideMenu;
+    Unbinder unbinder;
+    @BindView(R2.id.main_listview)
+    ListView mainListview;
+
     public static CehuaMainContent newInstance() {
-        
+
         Bundle args = new Bundle();
-        
+
         CehuaMainContent fragment = new CehuaMainContent();
         fragment.setArguments(args);
         return fragment;
     }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.customize_cehuamianban_mian, container, false);
+        View view = inflater.inflate(R.layout.customize_cehua, container, false);
+        unbinder = ButterKnife.bind(this, view);
         return view;
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        menuListview.setAdapter(new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, Constant.sCheeseStrings) {
+            @NonNull
+            @Override
+            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                TextView textView = (TextView) super.getView(position, convertView, parent);
+                textView.setTextColor(Color.WHITE);
+                return textView;
+            }
+        });
+        mainListview.setAdapter(new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, Constant.NAMES){
+            @NonNull
+            @Override
+            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                View view=super.getView(position, convertView, parent);
+                view.setScaleX(0.5f);
+                view.setScaleY(0.5f);
+                //以属性动画放大,ViewPropertyAnimator是新导入的jar包
+                ViewPropertyAnimator.animate(view).scaleX(1).setDuration(200).start();
+                ViewPropertyAnimator.animate(view).scaleY(1).setDuration(200).start();
+                return view;
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
 }
