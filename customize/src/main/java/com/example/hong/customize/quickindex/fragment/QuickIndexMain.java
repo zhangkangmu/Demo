7b96bdc48aa5c5c5ccbd7a137a5a2ba7b30d4ffb
zhangyuhong <289590351@qq.com>
2019-05-10 01:35:04 +0800

    填充listview列表，文字转拼音

--- customize/src/main/java/com/example/hong/customize/quickindex/fragment/QuickIndexMain.java
+++ customize/src/main/java/com/example/hong/customize/quickindex/fragment/QuickIndexMain.java
@@ -6,10 +6,17 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.example.hong.customize.R;
 import com.example.hong.customize.R2;
+import com.example.hong.customize.quickindex.adapter.MyAdapter;
+import com.example.hong.customize.quickindex.other.Friend;
 import com.example.hong.customize.quickindex.ui.QuickIndexBar;
+import com.example.hong.customize.utils.Utils;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -25,7 +32,10 @@ public class QuickIndexMain extends SupportFragment {
     @BindView(R2.id.quickIndexBar)
     QuickIndexBar quickIndexBar;
     Unbinder unbinder;
+    @BindView(R2.id.listview)
+    ListView listview;
 
+    private ArrayList<Friend> friends=new ArrayList<Friend>();
     public static QuickIndexMain newInstance() {
         Bundle args = new Bundle();
         QuickIndexMain fragment = new QuickIndexMain();
@@ -47,14 +57,51 @@ public class QuickIndexMain extends SupportFragment {
         quickIndexBar.setOnTouchListener(new QuickIndexBar.OnTouchListener() {
             @Override
             public void onTouchLetter(String letter) {
-                Log.d("zyh",letter);
+                Log.d("zyh", letter);
             }
         });
+        //1.准备数据
+        fillList();
+        //2.对数据进行排序
+        Collections.sort(friends);
+        //3.设置Adapter
+        listview.setAdapter(new MyAdapter(friends,getContext()));
+        Utils.getPinyin("张宇洪");
     }
+
+
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         unbinder.unbind();
+    }
+
+    private void fillList() {
+        // 虚拟数据
+        friends.add(new Friend("aa李伟"));
+        friends.add(new Friend("张三"));
+        friends.add(new Friend("阿三"));
+        friends.add(new Friend("阿四"));
+        friends.add(new Friend("段誉"));
+        friends.add(new Friend("段正淳"));
+        friends.add(new Friend("张三丰"));
+        friends.add(new Friend("陈坤"));
+        friends.add(new Friend("林俊杰1"));
+        friends.add(new Friend("陈坤2"));
+        friends.add(new Friend("王二a"));
+        friends.add(new Friend("林俊杰a"));
+        friends.add(new Friend("张四"));
+        friends.add(new Friend("林俊杰"));
+        friends.add(new Friend("王二"));
+        friends.add(new Friend("王二b"));
+        friends.add(new Friend("赵四"));
+        friends.add(new Friend("杨坤"));
+        friends.add(new Friend("赵子龙"));
+        friends.add(new Friend("杨坤1"));
+        friends.add(new Friend("李伟1"));
+        friends.add(new Friend("宋江"));
+        friends.add(new Friend("宋江1"));
+        friends.add(new Friend("李伟3"));
     }
 }
---
+++ customize/src/main/java/com/example/hong/customize/utils/Utils.java
@@ -0,0 +1,74 @@
+package com.example.hong.customize.utils;
+
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.text.TextUtils;
+import android.util.Log;
+
+import net.sourceforge.pinyin4j.PinyinHelper;
+import net.sourceforge.pinyin4j.format.HanyuPinyinCaseType;
+import net.sourceforge.pinyin4j.format.HanyuPinyinOutputFormat;
+import net.sourceforge.pinyin4j.format.HanyuPinyinToneType;
+import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombination;
+
+/**
+ * Created by 洪 on 2019/5/9.
+ * 张宇洪自己封装的一写工具类
+ */
+
+public class Utils {
+    /**
+     * 获取文本的高度
+     * @param text
+     * @return
+     */
+    public static float getTextHeight(String text, Paint paint) {
+        Rect rect = new Rect();
+        paint.getTextBounds(text,0,text.length(),rect);
+        return rect.height();
+    }
+
+    /**
+     * 获取汉字的拼音的方法
+     * 会消耗一定的资源，所以不应该被频繁调用
+     *需要引入pinyin4j.jar包
+     * @param chinese
+     * @return
+     */
+    public static String getPinyin(String chinese) {
+        if(TextUtils.isEmpty(chinese)) return null;
+        //用来设置转化的拼音的大小写，或者声调
+        HanyuPinyinOutputFormat hanyuPinyinOutputFormat = new HanyuPinyinOutputFormat();
+        hanyuPinyinOutputFormat.setCaseType(HanyuPinyinCaseType.UPPERCASE);   //设置转化的拼音是大写字母
+        hanyuPinyinOutputFormat.setToneType(HanyuPinyinToneType.WITHOUT_TONE);   //设置转化的拼音不带声调
+
+        //1.由于只能对单个汉字转化，所以需要将字符串转化为字符数组，然后对每个字符转化，最后拼接起来
+        char[] charArray = chinese.toCharArray();
+        String pinyiu="";
+        for(int i=0;i<charArray.length;i++){
+            //2.过滤空格   张    宇   洪 ->ZHANGYUHONG
+            if (Character.isWhitespace(charArray[i])) continue;
+            //3.需要判断是否是汉字   a@#张&*&宇%#洪
+            //汉字占2个字节，一个字节范围是-128~127，那么汉字肯定大于127
+            if(charArray[i]>127){     //charArray[i]也可以是字节！！！！！！！！！！！！！！！
+                //由于多音字的存在，比如单  dan shan，这个是不可避免的
+                try {
+                    String[] pinyinArr = PinyinHelper.toHanyuPinyinStringArray(charArray[i], hanyuPinyinOutputFormat);//拼音有音调之类的，因此要设置一个格式
+                    if (pinyinArr!=null){
+                        pinyiu+=pinyinArr[0];   //此处即使有多音字，那么也只能取第一个拼音
+                    }else {
+                        //说明没有找到对应的拼音，汉字有问题，或者可能不是汉字，则忽略
+                    }
+                } catch (BadHanyuPinyinOutputFormatCombination badHanyuPinyinOutputFormatCombination) {
+                    badHanyuPinyinOutputFormatCombination.printStackTrace();
+                    //说明转化失败，不是汉字，比如O(∩_∩)O~，那么则忽略
+                }
+            }else {
+                //肯定不是汉字，应该是键盘上能够直接输入的字符，这些字符能够排序，但不能获取拼音
+                //所以可以直接拼接  a张宇洪->
+                pinyiu += (charArray[i]+"").toUpperCase();
+            }
+        }
+        return pinyiu;
+    }
+}
